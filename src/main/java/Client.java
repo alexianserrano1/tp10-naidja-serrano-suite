4d24c5b2faea4204c3be84a7b5bb15827dc78846
s15037015 <alexian.serrano@gmail.com>
2018-12-11 08:43:28 +0100

    Fin

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,10 +1,15 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Client {
+public class Client implements Runnable{
     private String url;
     private int port;
     private Socket socket;
+    PrintWriter writer;
 
     public Client(String url, int port) throws IOException {
         this.url = url;
@@ -12,7 +17,26 @@ public class Client {
         socket = new Socket(url, port);
     }
 
-    void sendMessage(String message) {
+    void sendMessage(String message) throws IOException {
+        writer.println("["+message+"]");
+    }
 
+    void close() throws IOException {
+        socket.close();
+    }
+
+    public void run() {
+        try {
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(
+                            socket.getInputStream()));
+
+            for(;;)
+                System.out.println(reader.readLine());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
